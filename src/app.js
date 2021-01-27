8bf01b8574f2d84c7e466615eabac4567c8fe125
omodauda <omodauda.dl@gmail.com>
2021-01-27 13:52:23 +0100

    send errMsg for invalid JSON payload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import routes from './routes';
+import { errMsg } from './utils/response';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use((err, req, res, next) => {
+  // This check makes sure this is a JSON parsing issue
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return errMsg(res, "Invalid JSON payload passed.")
+  }
+  next();
+});
+
 app.use('/', routes);
 
 app.listen(3000, () => {
