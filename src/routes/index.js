96d323d6c1aafef99282af675d9f57377cc3ad38
omodauda <omodauda.dl@gmail.com>
2021-01-28 16:37:14 +0100

    create API routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { baseUrl} from '../controllers';
+import { 
+  baseUrl,
+  validationUrl
+} from '../controllers';
+import validateInput from '../validator/index';
 
 const router = new Router();
 
 router
   .route('/')
   .get(baseUrl)
+
+router
+  .route('/validate-rule')
+  .post(validateInput, validationUrl)
 
 export default router;
