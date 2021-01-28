db1c02b4b82fc13256f1b50e0631597a2d47ff85
omodauda <omodauda.dl@gmail.com>
2021-01-28 16:39:17 +0100

    resolve route controllers and responses

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,8 @@
+import {
+  successResponse,
+  errorResponse
+} from '../utils/response';
+
 const baseUrl = (req, res) => {
   const data = {
     name: "Lawal Dauda",
@@ -13,8 +18,77 @@ const baseUrl = (req, res) => {
     status: "success",
     data
   });
-}
+};
+
+const validationUrl = (req, res) => {
+  const { rule, data} = req.body;
+  const {field, condition, condition_value} = rule;
+  const split = field.split('.');
+  // console.log(split);
+
+  let fieldValue;
+
+  if(split.length === 1){
+    fieldValue = data[split[0]];
+    
+    if(condition === "eq"){
+      data[split[0]] === condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "neq"){
+      data[split[0]] !== condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "gt"){
+      condition_value > data[split[0]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    }
+    if(condition === "gte"){
+      condition_value >= data[split[0]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    }
+    if(condition === "contains"){
+      data[split[0]].includes(condition_value) ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    }
+  };
+
+  if(split.length === 2){
+    fieldValue = data[split[0]][split[1]];
+    if(condition === "eq"){
+      data[split[0]][split[1]] === condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "neq"){
+      data[split[0]][split[1]] !== condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "gt"){
+      condition_value > data[split[0]][split[1]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "gte"){
+      condition_value >= data[split[0]][split[1]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "contains"){
+      data[split[0]][split[1]].includes(condition_value) ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+  };
+
+  if(split.length === 3){
+    fieldValue = data[split[0]][split[1]][split[2]];
+    if(condition === "eq"){
+      data[split[0]][split[1]][split[2]] === condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "neq"){
+      data[split[0]][split[1]][split[2]] !== condition_value ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "gt"){
+      condition_value > data[split[0]][split[1]][split[2]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "gte"){
+      condition_value >= data[split[0]][split[1]][split[2]] ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+    if(condition === "contains"){
+      data[split[0]][split[1]][split[2]].includes(condition_value) ? successResponse(res, rule, field, fieldValue) : errorResponse(res, rule, field, fieldValue);
+    };
+  }
+  
+};
 
 export {
-  baseUrl
+  baseUrl,
+  validationUrl
 }
---
+++ src/utils/response.js
@@ -0,0 +1,52 @@
+const errMsg = (res, msg)=>{
+  return res
+  .status(400)
+  .json({
+    message: msg,
+    status: "error",
+    data: null
+  });
+};
+
+const successResponse = (res, {condition, condition_value}, field, fieldValue) => {
+  return res
+  .status(200)
+  .json({
+    message: `field ${field} successfully validated.`,
+    status: "success",
+    data: {
+      validation: {
+        error: "false",
+        field,
+        fieldValue,
+        condition,
+        condition_value
+      }
+    }
+  })
+};
+
+const errorResponse = (res, {condition, condition_value}, field, fieldValue) => {
+  // console.log('i was called')
+  return res
+  .status(400)
+  .json({
+    message: `field ${field} failed validation.`,
+    status: "error",
+    data: {
+      validation: {
+        error: "true",
+        field,
+        fieldValue,
+        condition,
+        condition_value
+      }
+    }
+  })
+};
+
+export {
+  errMsg,
+  successResponse,
+  errorResponse
+}
