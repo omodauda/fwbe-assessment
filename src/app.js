bc40f8feba887d51441f621022b85c28ab83eb06
omodauda <omodauda.dl@gmail.com>
2021-01-30 16:21:00 +0100

    prepare app for heroku

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use((err, req, res, next) => {
 
 app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log(`app is running on port 3000`)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`)
 })
